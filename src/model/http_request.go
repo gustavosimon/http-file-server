faa03349f7af6cabecc7e55ca0eb1bca330921df
Gustavo Simon <gustavo.andre.simon@hotmail.com>
2024-04-28 11:56:41 -0300

    feat: Implementado parser da requisição HTTP.

--- src/model/http_request.go
+++ src/model/http_request.go
@@ -1,5 +1,7 @@
 package model
 
+import "strings"
+
 // Estrutura de uma requisição HTTP
 type HttpRequest struct {
 	Method      string
@@ -11,15 +13,37 @@ type HttpRequest struct {
 
 // Método para criar um objeto HttpRequest a partir de um payload recebido pela requisição
 func CreateFromPayload(payload string) HttpRequest {
+	lines := strings.Split(payload, "\n")
+	method, resource, httpVersion := parseRequestLine(lines[0])
+	headers := parseHeaders(lines[1:])
 
-	// TODO: implementar a regra para construir o objeto HttpRequest
-
-	var headers = make(map[string]string)
-	headers["Host"] = "localhost:8080"
-	return HttpRequest{
-		Method:      "GET",
-		Resource:    "/",
-		HttpVersion: "1.1",
-		Headers:     headers,
+	body := ""
+	if len(lines) > 0 {
+		body = lines[len(lines)-1]
 	}
+
+	return HttpRequest{
+		Method:      method,
+		Resource:    resource,
+		HttpVersion: httpVersion,
+		Headers:     headers,
+		Body:        body,
+	}
+}
+
+func parseRequestLine(requestLine string) (method, resource, httpVersion string) {
+	parts := strings.Split(requestLine, " ")
+	return parts[0], parts[1], parts[2]
+}
+
+func parseHeaders(headerLines []string) map[string]string {
+	headers := make(map[string]string)
+	for _, line := range headerLines {
+		parts := strings.SplitN(line, ": ", 2)
+		if len(parts) < 2 {
+			continue
+		}
+		headers[parts[0]] = parts[1]
+	}
+	return headers
 }
